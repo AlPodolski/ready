576a70fc4681182c4c6bb1c10c632cdf614969b6
aleksandrpodolskij <sanyapodolski@gmail.com>
2025-11-07 18:17:55 +0200

    правки

--- resources/views/new/robot/index.blade.php
+++ resources/views/new/robot/index.blade.php
@@ -1,28 +1,31 @@
 User-agent: *
 Disallow: /
-Disallow: */?q=*
-Disallow: /filter?*
-Disallow: /poisk?*
-Allow: */*.js
-Allow: */*.css
-Allow: */*.gif
-Allow: */*.png
-Allow: */*.jpeg
-Allow: */*.jpg
-Allow: */*.JPG
-Allow: */*.woff
-Allow: */*.woff2
-Allow: */*.ttf
+@if(false)
+    Disallow: */?q=*
+    Disallow: /filter?*
+    Disallow: /poisk?*
+    Allow: */*.js
+    Allow: */*.css
+    Allow: */*.gif
+    Allow: */*.png
+    Allow: */*.jpeg
+    Allow: */*.jpg
+    Allow: */*.JPG
+    Allow: */*.woff
+    Allow: */*.woff2
+    Allow: */*.ttf
 
-User-agent: Yandex
-Disallow: /filter?*
-Disallow: /poisk?*
-Disallow: */amp/*
+    User-agent: Yandex
+    Disallow: /filter?*
+    Disallow: /poisk?*
+    Disallow: */amp/*
 
-Clean-param: p
+    Clean-param: p
 
-Disallow: /login
-Disallow: /register
+    Disallow: /login
+    Disallow: /register
 
-Sitemap: https://{{$host}}/sitemap_post.xml
-Sitemap: https://{{$host}}/sitemap_page.xml
+    Sitemap: https://{{$host}}/sitemap_post.xml
+    Sitemap: https://{{$host}}/sitemap_page.xml
+@endif
+
